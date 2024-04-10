d33bca27b4e56df135f073d2567ee6f37c4a9874
Yerassyl Orazkhan <y_orazkhan@kbtu.kz>
2024-04-10 22:20:55 +0600

    1

--- Lab6/albums/src/app/albums/albums.component.ts
+++ Lab6/albums/src/app/albums/albums.component.ts
@@ -1,21 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Album } from '../album';
 import { AlbumsService } from '../albums.service';
+
 
 @Component({
   selector: 'app-albums',
   templateUrl: './albums.component.html',
   styleUrl: './albums.component.css'
 })
-export class AlbumsComponent {
+export class AlbumsComponent implements OnInit {
   albums!: Album[];
-
+  startIndex: number = 0;
+  endIndex: number = 10;
+  
   constructor(private albumService: AlbumsService){}
 
-  ngOnInit(){
+  ngOnInit() {
     this.albumService.getAlbums().subscribe((albums) => {
       this.albums = albums;
-    })
+      this.updateDisplayedAlbums();
+    });
+  }
+
+  updateDisplayedAlbums() {
+    this.albums = this.albums.slice(this.startIndex, this.endIndex);
   }
 
   deleteAlbum(id: number){
